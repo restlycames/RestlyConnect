c7c9902074b7dbe9cba16fafb3d5172b2a83713d
restlycames <demastandoff22@gmail.com>
2026-02-23 18:26:56 +0300

    Update sub.js

--- vercel-sub/api/sub.js
+++ vercel-sub/api/sub.js
@@ -1,107 +1,103 @@
 module.exports = (req, res) => {
   const config = [
-    {
-      "burstObservatory": {
-        "pingConfig": {
-          "destination": "https://www.google.com/generate_204",
-          "interval": "8s",
-          "sampling": 2,
-          "timeout": "3s"
+  {
+    "dns": {
+      "queryStrategy": "UseIPv4",
+      "servers": [
+        "https+local://1.1.1.1/dns-query",
+        "https+local://8.8.8.8/dns-query",
+        "77.88.8.8"
+      ]
+    },
+    "inbounds": [
+      {
+        "listen": "127.0.0.1",
+        "port": 10808,
+        "protocol": "socks",
+        "settings": {
+          "udp": true
         },
-        "subjectSelector": ["ru1","ru2","ru3","ru4","ru5"]
+        "tag": "socks"
       },
-      "dns": {
-        "queryStrategy": "UseIPv4",
-        "servers": [
-          "https+local://1.1.1.1/dns-query",
-          "https+local://8.8.8.8/dns-query",
-          "77.88.8.8"
-        ]
-      },
-      "inbounds": [
-        {
-          "listen": "127.0.0.1",
-          "port": 10808,
-          "protocol": "socks",
-          "settings": { "udp": true },
-          "tag": "socks"
-        },
-        {
-          "listen": "127.0.0.1",
-          "port": 10809,
-          "protocol": "http",
-          "tag": "http"
-        }
-      ],
-      "meta": null,
-      "outbounds": [
-        {
-          "protocol": "vless",
-          "settings": {
-            "vnext": [{
+      {
+        "listen": "127.0.0.1",
+        "port": 10809,
+        "protocol": "http",
+        "tag": "http"
+      }
+    ],
+    "meta": null,
+    "outbounds": [
+      {
+        "tag": "russ1",
+        "protocol": "vless",
+        "settings": {
+          "vnext": [
+            {
               "address": "51.250.86.102",
               "port": 443,
-              "users": [{
-                "encryption": "none",
-                "flow": "xtls-rprx-vision",
-                "id": "53b2c46d-bc0a-4696-9d87-6bfa77a60d1f"
-              }]
-            }]
-          },
-          "streamSettings": {
-            "network": "tcp",
-            "realitySettings": {
-              "fingerprint": "chrome",
-              "publicKey": "FkmYFobwxLMLEktYXywmjthuEYCZggITsxwPNasTKUg",
-              "serverName": "api-maps.yandex.ru",
-              "shortId": "33554379411b3219"
-            },
-            "security": "reality"
-          },
-          "tag": "ru1"
+              "users": [
+                {
+                  "id": "53b2c46d-bc0a-4696-9d87-6bfa77a60d1f",
+                  "flow": "xtls-rprx-vision",
+                  "encryption": "none"
+                }
+              ]
+            }
+          ]
         },
-        { "protocol": "freedom", "tag": "direct" },
-        { "protocol": "blackhole", "tag": "block" }
-      ],
-      "remarks": "Config 1",
-      "routing": {
-        "balancers": [{
-          "fallbackTag": "ru1",
-          "selector": ["ru1","ru2","ru3","ru4","ru5"],
-          "strategy": {
-            "settings": {
-              "baselines": ["2s"],
-              "expected": 1,
-              "maxRTT": "3s",
-              "tolerance": 0.3
-            },
-            "type": "leastLoad"
-          },
-          "tag": "auto_ru"
-        }],
-        "domainStrategy": "IPIfNonMatch",
-        "rules": [
-          {
-            "outboundTag": "block",
-            "protocol": ["bittorrent"],
-            "type": "field"
-          },
-          {
-            "balancerTag": "auto_ru",
-            "inboundTag": ["socks","http"],
-            "network": "tcp,udp",
-            "type": "field"
+        "streamSettings": {
+          "network": "tcp",
+          "security": "reality",
+          "realitySettings": {
+            "publicKey": "FkmYFobwxLMLEktYXywmjthuEYCZggITsxwPNasTKUg",
+            "shortId": "33554379411b3219",
+            "spiderX": "",
+            "serverName": "api-maps.yandex.ru",
+            "fingerprint": "qq",
+            "show": false
           }
-        ]
+        }
+      },
+      {
+        "protocol": "freedom",
+        "tag": "direct"
+      },
+      {
+        "protocol": "blackhole",
+        "tag": "block"
       }
+    ],
+    "remarks": "🇷🇺 Russia | Auto",
+    "routing": {
+      "domainStrategy": "IPIfNonMatch",
+      "rules": [
+        {
+          "outboundTag": "block",
+          "protocol": [
+            "bittorrent"
+          ],
+          "type": "field"
+        },
+        {
+          "outboundTag": "russ1",
+          "inboundTag": [
+            "socks",
+            "http"
+          ],
+          "network": "tcp,udp",
+          "type": "field"
+        }
+      ]
     }
-  ];
+  }
+];
 
   res.setHeader("Content-Type", "application/json; charset=utf-8");
   res.setHeader("profile-title", "RestlyConnect");
   res.setHeader("support-url", "https://t.me/restlyconnect");
   res.setHeader("profile-update-interval", "24");
-  res.setHeader("announce", "UPD: 23.02.2026 15:58 | tgk: @restlyconnect");
+  res.setHeader("announce", "UPD: 23.02.2026 18:24 | tgk: @restlyconnect");
   res.setHeader("Access-Control-Allow-Origin", "*");
 
   res.end(JSON.stringify(config));
